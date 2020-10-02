9120c6de0c513c1471aad9150012b6806da1c531
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-02 13:00:54 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -3,10 +3,10 @@ name=input("Enter a name: ")
 value=name in nameList
 for name in nameList:
     if value==True:
-        print("That name is on the list.")
+        print("That name is on the list")
         break
     else: 
-        print("That name is not on the list.")
+        print("That name is not on the list")
         break
 
     
