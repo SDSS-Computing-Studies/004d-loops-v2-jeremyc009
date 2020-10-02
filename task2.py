d324560281cac3c9e804577a0bb5594248b2797f
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-02 12:58:59 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
-nameList=("Lebron", "Kobe","Michael","Shaq","Dennis","Elmo")
+nameList=("Lebron", "Kobe","Michael","Shaq","Dennis",)
 name=input("Enter a name: ")
 value=name in nameList
 for name in nameList:
