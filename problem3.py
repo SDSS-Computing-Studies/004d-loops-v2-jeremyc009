8e259a5064a8346a8a37d546702496ac56317fbb
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-02 12:54:25 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -2,12 +2,19 @@ num=int(input("Enter an integer smaller than 10: "))
 if num>=10:
     print("That integer is larger than 10")
     quit()
-num1=0
-count=0
-numList=(1,11,111,1111,11111,111111,1111111,11111111,111111111)
-while count<4:
-    count=count+1
-    for i in numList:
-        
-print(num3)
+digit="1"
+a=0
+b=0
+c=0
+d=0
+for i in range(1,num+1):
+    
+    b=int(i*digit)
+    d=b+c
+    c=d
+    
+    
+print(d)
 
+    
+    
